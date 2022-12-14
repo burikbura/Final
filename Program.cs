04a505941f0b9d483f9ebf8196f4e3750f9ef78a
Burik <burtsevjr04@mail.ru>
2022-12-14 17:09:45 +0300

    Создали массив заполненный пользователем

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿string[] addArray()
+{
+
+  Console.Write($"Введи кол-во элементов массива:\t");
+  int elmCount = int.Parse(Console.ReadLine());
+  string[] myArray = new string[elmCount];
+  for (int i = 0; i < myArray.Length; i++)
+  {
+    Console.WriteLine($" \n Введи значение для {i}-го элемента массива: ");
+    myArray[i] = Console.ReadLine();
+  }
+  return myArray;
+}
+
