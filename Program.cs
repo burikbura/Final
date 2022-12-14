6da287dac479aa10b7f66d902c92bb514a67bb66
Burik <burtsevjr04@mail.ru>
2022-12-14 17:10:55 +0300

    Выводим массив на экран

--- Program.cs
+++ Program.cs
@@ -12,3 +12,19 @@
   return myArray;
 }
 
+// 2. Вывод на экран массива, созданного юзером
+void printBaseArray(string[] myArray)
+{
+  Console.WriteLine($" \n Массив, созданный юзером: ");
+
+  Console.Write($"[");
+  for (int i = 0; i < myArray.Length; i++)
+  {
+    Console.Write($" {myArray[i],1} ");
+  }
+  Console.Write($"] \n");
+
+  Console.WriteLine(" \n ============== ");
+}
+
+
